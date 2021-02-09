d5181a4cb8b2d592f0f06b5a4cae01585b29f738
LiquidDemAn <dimonshinkarenko2001@gmail.com>
2021-02-09 16:04:49 +0200

    creat slider and fix some problems

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+$(document).ready(function(){
+    $('.welcome-slider__wrapper').slick({
+        slidesToShow: 1,
+        adaptiveHeight: true,
+    });
+    $('.welcome-thumbnails').slick({
+        arrows: false,
+        slidesToShow: 6,
+        asNavFor: '.welcome-slider__wrapper',
+        focusOnSelect: true
+    });
+});
